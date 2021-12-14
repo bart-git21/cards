9e9774276c9fa26bffaf64b017b817feecccad70
gitName <bartk@list.ru>
2021-12-14 10:19:49 +0300

    fix view-button (should be hidden)

--- script.js
+++ script.js
@@ -3,4 +3,7 @@
 function open_close_button() {
     let profile = document.querySelector(".content-box");
     profile.classList.toggle("visible");
+
+    let view_button = document.querySelector(".view-button");
+    view_button.classList.toggle("hidden");
 }
